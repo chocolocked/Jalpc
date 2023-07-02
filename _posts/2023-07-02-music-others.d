30bd3603ecabd6329e57fb12b47a4050d8f784fc
chocolocked <GinnyChu512@gmail.com>
2023-07-02 14:47:20 -0400

    Create 2023-07-02-music-others.md

---
+++ _posts/2023-07-02-music-others.d
@@ -0,0 +1,55 @@
+---
+layout: post
+title:  "music & others"
+date:   2023-07-02
+desc: "idk"
+keywords: "music,harrypotter, blog,easy,update"
+categories: [Ideas]
+tags: [Ideas,Chocolocked]
+icon: icon-lightbulb
+
+---
+
+2nd Ed Sheeran concert of the year, @Foxborough, Gillette Stadium.
+
+The light dimmed, the crowd got quiet. This red hair guy was standing still striking the guitar, singing with his eyes closed. 
+Everything else disappeared from this world, all that was left was his melody, my memories, and my thoughts. 
+
+'''
+We keep this love in a photograph
+
+We made these memories for ourselves
+
+Where our eyes are never closing
+
+Hearts are never broken
+
+And time's forever frozen, still
+'''
+
+We all have been there; loved and hurt, didn't love and hurt others. We are all imperfect human beings learning how to let go of past relationships,
+learning to become better, and learning to trust and be open to another person with the possibility of getting hurt again. Maybe I'm a hopeless romantic deep down.  
+“Love is totally nonsensical. But we have to keep doing it or else we’re lost and love is dead, and humanity should just pack it in. 
+Because love is the best thing we do.”
+One day I'll be getting married (I know that), and WE WILL be getting married to this song. 
+[![Spotify](https://spotify-github-readme.vercel.app/api/spotify)](https://open.spotify.com/track/1HNkqx9Ahdgi1Ixy2xkKkL)
+
+D and I were lying down in the car seats while waiting in a line to get out of the parking lot that seemed never moving and randomly chatting about
+hopes and dreams. The moon was shining bright.
+"I want to live in a wooden cottage house in a secluded forest with lots of books and still keep my writing habit when I'm 80. You know, maybe somewhere
+in the NC countryside", she said. 
+"Just you? Don't you want to be with somebody?", I asked, "I want to live somewhere near the water with lots of sunshine, with someone I love
+and dog(s). And we can read, dance, sing, cook, and laugh all the time. I think I'll retire somewhere in Europe".
+
+"What's your Hogwarts house?"
+"Ravenclaw, yours?" I asked.
+"Hufflepuff, I'm so surprised cuz I thought you would be a Gryffindor, cuz you're so ... brave". 
+"I would like to be Gryffindor but Pottermore clearly decided I wasn't that brave enough. Maybe Pottermore is right".
+
+We laughed. 
+
+
+
+
+
+
